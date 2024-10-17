88389cc0a951995c38f32a671c61abb76d019580
ysksms <ysk06.sss@gmail.com>
2024-10-17 16:25:32 -0700

    add  hangman.py

---
+++ hangman.py
@@ -0,0 +1,13 @@
+# main function to start the game
+def main():
+    """
+    Main function to load words, select a random word, and start the Hangman game.
+    """
+    # words = load_words("words.txt")
+    # word = choose_random_word(words)
+    # hangman_game(word)
+
+
+# run the game
+if __name__ == "__main__":
+    main()
